c37378a47065f7b2058d36625ee349720cd422e2
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-19 13:02:40 +0700

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const permissions =  require("../../permissions.json");
-const { security, cors, cookies, compression, messages, auth, missing, catchAll } = require("@ndiinginc/router");
+const { security, cors, cookies, compression, messages, missing, catchAll } = require("@ndiinginc/router");
 
 const router = express.Router();
 
-router.use(security(), cors(), cookies(), compression(), messages(), auth(permissions));
+router.use(security(), cors(), cookies(), compression(), messages());
 router.use("/main", require("./main/index.js"));
 router.use(missing(), catchAll());
 
