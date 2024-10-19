4697dbc829ce5e4b02025cf44539f915c494a154
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-19 22:18:35 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -23,19 +23,19 @@ function handleConnection(socket, req) {
     socket.on("message", console.log);
 }
 
+function handleUpgrade(ws) {
+    return (req, socket, head) => {
+        ws.handleUpgrade(req, socket, head, (sock) => {
+            ws.emit("connection", sock, req);
+        });
+    };
+}
+
 ws.on("connection", handleConnection);
 wss.on("connection", handleConnection);
 
-httpServer.on("upgrade", (req, socket, head) => {
-    ws.handleUpgrade(req, socket, head, (sock) => {
-        ws.emit("connection", sock, req);
-    });
-});
-httpsServer.on("upgrade", (req, socket, head) => {
-    wss.handleUpgrade(req, socket, head, (sock) => {
-        wss.emit("connection", sock, req);
-    });
-});
+httpServer.on("upgrade", handleUpgrade(ws));
+httpsServer.on("upgrade", handleUpgrade(wss));
 
 httpServer.listen(process.env.HTTP_PORT, "0.0.0.0", () => console.log(httpServer.address()));
 httpsServer.listen(process.env.HTTPS_PORT, "0.0.0.0", () => console.log(httpsServer.address()));
